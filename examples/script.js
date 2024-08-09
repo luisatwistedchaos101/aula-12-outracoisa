64a055c4c9150e52f6d08915eb592d8a30bc4900
wallacevieira <wallaceofc@hotmail.com>
2024-08-09 13:44:58 -0300

    first commit

---
+++ examples/script.js
@@ -0,0 +1,61 @@
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const mapped = arr.map(item => item * 2);
+console.log(mapped);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filtered = arr.filter(item => item % 2 === 0);
+console.log(filtered);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sum = arr.reduce((accumulator, item) => accumulator + item);
+console.log(sum);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const found = arr.find(item => item === 2);
+console.log(found);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const foundIndex = arr.findIndex(item => item > 3);
+console.log(foundIndex);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const found = arr.some(item => item === 2);
+console.log(found);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const iterated = arr.forEach(item => consolo.log(item));
+console.log(iterated);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const allEven = arr.every(item => item % 2 === 0);
+console.log(allEven);
+*/
+
+/* 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < arr.length, i++) {
+  console.log(arr[i]);
+}
+*/
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const item of arr) {
+  console.log(item);
+}
+
